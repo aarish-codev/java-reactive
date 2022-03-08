68c54a144d71f97bfce7184cf6f46549645ae7a7
AarishS <aarish.work@gmail.com>
2022-03-08 20:54:16 +0530

    onError & Runnable demo

--- src/main/java/com/reactive/mono/MonoDemo.java
+++ src/main/java/com/reactive/mono/MonoDemo.java
@@ -9,8 +9,30 @@ public class MonoDemo {
         // publisher
         Mono<Integer> mono = Mono.just(1);
         System.out.println(mono);
+        System.out.println("-------------------------------------------");
 
         //subscriber
         mono.subscribe(i-> System.out.println("Received : "+i));
+        System.out.println("-------------------------------------------");
+
+        //subscriber with error
+        mono.subscribe(
+                item-> System.out.println("Received : "+item),
+                err -> System.out.println(err.getMessage()),
+                ()-> System.out.println("Completed" )
+        );
+        System.out.println("-------------------------------------------");
+
+        //noinspection divzero
+        Mono<Integer> integerMono = Mono.just("ball")
+                .map(String::length)
+                .map(l-> l/0);
+
+        //subscriber with onError and runnable
+        integerMono.subscribe(
+                item-> System.out.println("Received : "+item),
+                System.out::println,
+                ()-> System.out.println("Completed" )
+        );
     }
 }
