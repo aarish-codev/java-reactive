7777f9b1aa7ce445a4e388dde770e1933b7e1449
Aarish S <aarish.work@gmail.com>
2022-04-06 20:39:25 +0530

    hot & cold publisher demo

---
+++ src/main/java/com/reactive/publishers/ColdPublisherDemo.java
@@ -0,0 +1,31 @@
+package com.reactive.publishers;
+
+import com.reactive.util.Commons;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+public class ColdPublisherDemo {
+
+    public static void main(String[] args) {
+        Flux<String> movieStream = Flux.fromStream(ColdPublisherDemo::getMovie).delayElements(Duration.ofSeconds(2));
+
+        movieStream.subscribe(Commons.subscriber("sam"));
+
+        Commons.sleepSeconds(5);
+
+        movieStream.subscribe(Commons.subscriber("mike"));
+
+
+        Commons.sleepSeconds(60);
+
+
+    }
+    
+    private static Stream<String> getMovie() {
+        System.out.println("Got the movie streaming req");
+        return Stream.of("Scene 1", "Scene 2", "Scene 3", "Scene 4", "Scene 5", "Scene 6", "Scene 7");
+    }
+
+}
